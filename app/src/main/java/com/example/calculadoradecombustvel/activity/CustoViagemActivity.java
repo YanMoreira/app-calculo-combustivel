b71619642b97d7f00a6a43e2afea305fbeecba98
YanMoreira <yan@pc.unix.com.br>
2020-04-27 12:56:17 -0400

    Iniciando estrutura de código para tela de viagem

--- app/src/main/java/com/example/calculadoradecombustvel/activity/CustoViagemActivity.java
+++ app/src/main/java/com/example/calculadoradecombustvel/activity/CustoViagemActivity.java
@@ -6,17 +6,27 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.calculadoradecombustvel.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class CustoViagemActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private TextInputEditText editTextKmTotal, editTextGasolinaViagem, editTextEtanolViagem,
+            editTextKmlGasolinaViagem, editTextKmlEtanolViagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custo_viagem);
+
+        editTextKmTotal = findViewById(R.id.editTextKmTotal);
+        editTextGasolinaViagem = findViewById(R.id.editTextGasolinaViagem);
+        editTextEtanolViagem = findViewById(R.id.editTextEtanolViagem);
+        editTextKmlGasolinaViagem = findViewById(R.id.editTextKmlGasolinaViagem);
+        editTextKmlEtanolViagem = findViewById(R.id.editTextKmlEtanolViagem);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,7 +42,27 @@ public class CustoViagemActivity extends AppCompatActivity {
         });
 
 
-        }
+    }
+
+    public void botaoCalcularViagem(View view) {
+
+        String kmTotal = editTextKmTotal.getText().toString();
+        String gasolinaViagem = editTextGasolinaViagem.getText().toString();
+        String etanolViagem = editTextEtanolViagem.getText().toString();
+        String gasolinaKmlViagem = editTextKmlGasolinaViagem.getText().toString();
+        String etanolKmlViagem = editTextKmlEtanolViagem.getText().toString();
+
+
+    }
+
+    public String validarCampos(String kmTotal, String gasolinaViagem, String etanolViagem,
+        String gasolinaKmlViagem, String etanolKmlViagem) {
+
+
+
+        return "";
+    }
+
 
 }
 
